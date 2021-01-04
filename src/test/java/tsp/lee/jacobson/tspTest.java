1894afea6573e9354c3c2ada3808c2e6e793264e
Atangana57 <olivieratangana57@gmail.com>
2021-01-04 15:17:31 +0100

    thomas olivier premierefonction

---
+++ src/test/java/tsp/lee/jacobson/tspTest.java
@@ -0,0 +1,19 @@
+package tsp.lee.jacobson;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class tspTest {
+
+	@Test
+	
+    public void distanceTo(City city) {
+		boolean expected=true;
+		boolean actual=distanceTo;
+		assertequals:(expected, actual )
+		
+		fail("Not yet implemented");
+	}
+
+}
